bf30db747f2fe668d475e427535cc17cf88926e2
Olivier Mornard <olivier@mornard-o.nom.fr>
2013-09-03 17:42:09 +0200

    Apply patch Row chart enhancements
    Conflicts:
    	src/row-chart.js

--- src/row-chart.js
+++ src/row-chart.js
@@ -11,12 +11,37 @@ dc.rowChart = function (parent, chartGroup) {
     var _rowCssClass = "row";
 
     var _chart = dc.marginable(dc.colorChart(dc.baseChart({})));
+    
+    var _xScale;
 
     var _x;
 
     var _elasticX;
 
     var _xAxis = d3.svg.axis().orient("bottom");
+
+    var _rowsCap = Infinity;
+
+    var _othersLabel = "Others";
+
+    var _othersHandler = function (data, value) {
+        data.push({"key": _othersLabel, "value": value });
+    };
+
+    var _rowData = null;
+
+    function assembleData() {
+        if (_rowsCap == Infinity) {
+            _rowData = _chart.computeOrderedGroups(); // ordered by keys
+        } else {
+            var topRows = _chart.group().top(_rowsCap); // ordered by value
+            var topRowsSum = d3.sum(topRows, _chart.valueAccessor());
+            var allRows = _chart.group().all();
+            var allRowsSum = d3.sum(allRows, _chart.valueAccessor());
+            _othersHandler(topRows,allRowsSum - topRowsSum);
+            _rowData = topRows;
+        }
+    }
 
     function calculateAxisScale() {
         if (!_x || _elasticX) {
@@ -43,13 +68,21 @@ dc.rowChart = function (parent, chartGroup) {
     }
 
     _chart.doRender = function () {
+        assembleData();
+        _xScale = d3.scale.linear().domain([0, d3.max(_rowData, _chart.valueAccessor())]).range([0, _chart.effectiveWidth()]);
+ 
         _chart.resetSvg();
 
         _g = _chart.svg()
             .append("g")
             .attr("transform", "translate(" + _chart.margins().left + "," + _chart.margins().top + ")");
 
-        drawAxis();
+        _xAxis.scale(_xScale);
+
+        _g.append("g").attr("class", "axis")
+                        .attr("transform", "translate(0, " + _chart.effectiveHeight() + ")")
+                        .call(_xAxis);
+
         drawGridLines();
         drawChart();
 
@@ -87,13 +120,10 @@ dc.rowChart = function (parent, chartGroup) {
     }
 
     function drawChart() {
-        drawAxis();
-        drawGridLines();
-
         var rows = _g.selectAll("g." + _rowCssClass)
-            .data(_chart.group().all());
+                     .data(_rowData);
 
-        createElements(rows);
+        createElements(rows, _rowData);
         removeElements(rows);
         updateElements(rows);
     }
@@ -116,7 +146,7 @@ dc.rowChart = function (parent, chartGroup) {
     }
 
     function updateElements(rows) {
-        var height = rowHeight();
+        var n = _rowData.length;
 
         var rect = rows.attr("transform",function (d, i) {
                 return "translate(0," + ((i + 1) * _gap + i * height) + ")";
@@ -136,10 +166,6 @@ dc.rowChart = function (parent, chartGroup) {
                 var start = _x(0) == -Infinity ? _x(1) : _x(0);
                 return Math.abs(start - _x(_chart.valueAccessor()(d)));
             })
-            .attr("transform", translateX);
-
-        createTitles(rows);
-        updateLabels(rows);
     }
 
     function createTitles(rows) {
@@ -163,19 +189,22 @@ dc.rowChart = function (parent, chartGroup) {
             var lab = rows.select("text")
                 .attr("x", _labelOffsetX)
                 .attr("y", _labelOffsetY)
+                .on("click", onClick)
                 .attr("class", function (d, i) {
                     return _rowCssClass + " _" + i;
                 })
                 .text(function (d) {
-                    return _chart.label()(d);
+                            if (_chart.valueAccessor()(d) > 0) {
+                                return _chart.label()(d);
+                            } else {
+                                return "";
+                            }
                 });
-            dc.transition(lab, _chart.transitionDuration())
-            .attr("transform", translateX);
         }
     }
 
     function numberOfRows() {
-        return _chart.group().all().length;
+        return _rowData.length;
     }
 
     function rowHeight() {
@@ -229,6 +258,24 @@ dc.rowChart = function (parent, chartGroup) {
 
     _chart.isSelectedRow = function (d) {
         return _chart.hasFilter(_chart.keyAccessor()(d));
+    };
+
+    _chart.rowsCap = function (_) {
+        if (!arguments.length) return _rowsCap;
+        _rowsCap = _;
+        return _chart;
+    };
+
+    _chart.othersLabel = function (_) {
+        if (!arguments.length) return _othersLabel;
+        _othersLabel = _;
+        return _chart;
+    };
+
+    _chart.othersHandler = function (_) {
+        if (!arguments.length) return _othersHandler;
+        _othersHandler = _;
+        return _chart;
     };
 
     return _chart.anchor(parent, chartGroup);
